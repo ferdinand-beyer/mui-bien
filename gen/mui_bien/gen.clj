c8e94d3d33593710b70ca42e4e00c4918ce37128
Ferdinand Beyer <fbeyer@thoughtworks.com>
2021-04-30 09:25:55 +0200

    Skip existing namespaces in code generation

--- gen/mui_bien/gen.clj
+++ gen/mui_bien/gen.clj
@@ -5,6 +5,7 @@
             [clojure.string :as str]
             [camel-snake-kebab.core :as csk]))
 
+(def ^:dynamic *src-dir* "src")
 (def ^:dynamic *out-dir* "build/src")
 (def ^:dynamic *verbose* false)
 
@@ -29,14 +30,6 @@
          (list :require '[reagent.core :as r] [js-name :as module]))
    (list 'def clj-name (list 'r/adapt-react-class (symbol (name module) "default")))])
 
-(defn write-clj-file [ns forms]
-  (let [filename (str *out-dir* "/" (ns->filename ns))]
-    (io/make-parents filename)
-    (with-open [writer (io/writer filename)]
-      (println "Writing:" filename)
-      (doseq [form forms]
-        (pprint form writer)))))
-
 (defn package-forms [ns mod-names]
   (concat
    [(list 'ns ns
@@ -45,26 +38,33 @@
    (for [{:keys [ns clj-name]} mod-names]
      (list 'def clj-name (symbol (name ns) (name clj-name))))))
 
+(defn write-clj-file [ns forms-fn]
+  (let [base-filename (ns->filename ns)
+        src-filename (str *src-dir* "/" base-filename)]
+    (if (.exists (io/file src-filename))
+      (println "Exists:" src-filename)
+      (let [filename (str *out-dir* "/" base-filename)
+            forms (forms-fn)]
+        (io/make-parents filename)
+        (with-open [writer (io/writer filename)]
+          (println "Writing:" filename)
+          (doseq [form forms]
+            (pprint form writer)))))))
+
 (defn generate-module [{:keys [ns] :as names}]
-  (write-clj-file ns (module-forms names)))
+  (write-clj-file ns #(module-forms names)))
 
 (defn generate-package [package mod-names]
   (let [ns (symbol (str "mui-bien." package ".all"))]
-    (write-clj-file ns (package-forms ns mod-names))))
+    (write-clj-file ns #(package-forms ns mod-names))))
 
 (defn generate-modules [modules]
-  (doseq [[package [gen-mods provided-mods]] modules]
-    (let [gen-mod-names
-          (for [module gen-mods]
-            (let [names (names package module)]
-              (generate-module names)
-              names))
-
-          mod-names (->> provided-mods
-                         (map (partial names package))
-                         (concat gen-mod-names)
-                         (sort-by :clj-name))]
-      (generate-package package mod-names))))
+  (doseq [[package mods] modules]
+    (->> (for [module mods]
+           (let [names (names package module)]
+             (generate-module names)
+             names))
+         (generate-package package))))
 
 (defn generate
   ([] (generate nil))
