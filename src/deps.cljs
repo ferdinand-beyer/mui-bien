a289d98f6dccadb71a3b9bf24ebf59d8a1a7394d
Ferdinand Beyer <fbeyer@thoughtworks.com>
2021-05-03 20:13:24 +0200

    Upgrade material-ui and react dependencies

--- src/deps.cljs
+++ src/deps.cljs
@@ -1,4 +1,5 @@
-{:npm-deps {"react" "17.0.1"
-            "@material-ui/core" "4.11.0"
-            "@material-ui/icons" "4.4.1"
-            "@material-ui/lab" "4.0.0-alpha.49"}}
+{:npm-deps {"react" "^17.0.2"
+            "react-dom" "^17.0.2"
+            "@material-ui/core" "^4.11.4"
+            "@material-ui/icons" "^4.11.2"
+            "@material-ui/lab" "^4.0.0-alpha.58"}}
