de1aedc1356a409487b3baa1ce267b9866be65e4
Ferdinand Beyer <fbeyer@thoughtworks.com>
2021-05-04 22:01:25 +0200

    Add badge

---
+++ src/mui_bien/core/badge.cljs
@@ -0,0 +1,8 @@
+(ns mui-bien.core.badge
+  (:require [reagent.core :as r]
+            ["@material-ui/core/Badge" :as Badge]))
+
+(def -badge (r/adapt-react-class Badge/default))
+
+(defn badge [{:keys [badge-content] :as props}]
+  [-badge (assoc props :badge-content (r/as-element badge-content))])
