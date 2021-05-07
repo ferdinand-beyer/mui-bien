6688cc7d7e3cf8f58d64693de0956f731f1ae036
Ferdinand Beyer <fb@fbeyer.com>
2021-05-07 22:56:47 +0200

    Preserve string keys

--- src/mui_bien/interop.cljs
+++ src/mui_bien/interop.cljs
@@ -12,11 +12,30 @@
   ;; FIXME component will now receive props in camelCase...
   (-> component r/reactify-component hoc r/adapt-react-class))
 
-(defn map->obj [m]
-  (clj->js (transform-keys ->camelCaseString m)))
+(defn ->obj-key
+  "Transforms a Clojure key for a JavaScript object."
+  [k]
+  (if (keyword? k)
+    (->camelCaseString k)
+    k))
 
-(defn obj->map [obj]
-  (transform-keys ->kebab-case-keyword (js->clj obj)))
+(defn ->map-key
+  "Transforms a JavaScript key for a Clojure map."
+  [k]
+  (if (and (string? k)
+           (re-matches #"[A-Za-z]\w*" k))
+    (->kebab-case-keyword k)
+    k))
+
+(defn map->obj
+  "Transforms a Clojure Map to a JavaScript object."
+  [m]
+  (clj->js (transform-keys ->obj-key m)))
+
+(defn obj->map
+  "Transforms a JavaScript object to a Clojure map."
+  [obj]
+  (transform-keys ->map-key (js->clj obj)))
 
 (defn obj->map->obj
   "Returns a function that takes a JavaScript object, calls f
@@ -27,10 +46,12 @@
     (-> (obj->map obj) (f) (map->obj))))
 
 (defn wrap-element-props
-  "Wraps props that require element values with as-element."
-  [props wrap-key?]
+  "Wraps props that require element values with as-element, according
+   to (pred k).  One common idiom is to use a set as pred, e.g. to
+   map a known set of 'node' properties."
+  [props pred]
   (reduce (fn [props [k v]]
-            (if (wrap-key? k)
+            (if (pred k)
               (assoc props k (r/as-element v))
               props))
           {}
--- test/mui_bien/interop_test.cljs
+++ test/mui_bien/interop_test.cljs
@@ -1,5 +1,5 @@
 (ns mui-bien.interop-test
-  (:require [cljs.test :refer-macros [deftest is]]
+  (:require [cljs.test :refer-macros [deftest is are]]
             [mui-bien.interop :as interop]))
 
 (def map-obj-pairs
@@ -18,8 +18,31 @@
 
 (deftest test-map->obj
   (doseq [[map obj] map-obj-pairs]
-    (is (= (js->clj obj) (js->clj (interop/map->obj map))))))
+    (is (= (js->clj obj) (js->clj (interop/map->obj map)))))
+  
+  (is (= ["&:hover span"]
+         (-> {"&:hover span" {}}
+             (interop/map->obj)
+             (js-keys)
+             (js->clj)))
+      "preserves string keys"))
 
 (deftest test-obj->map
   (doseq [[map obj] map-obj-pairs]
-    (is (= map (interop/obj->map obj)))))
+    (is (= map (interop/obj->map obj))))
+  
+  (are [exp key] (= {exp {}}
+                    (interop/obj->map (clj->js {key {}})))
+    :lowercase "lowercase"
+    :camel-case "camelCase"
+    :pascal-case "PascalCase"
+    :uppercase "UPPERCASE"
+    :the-answer-is-42 "theAnswerIs42")
+  
+  (are [key] (= {key {}}
+                (interop/obj->map (clj->js {key {}})))
+    "42"
+    "contains space"
+    "nonWord-Character"
+    " leadingSpace"
+    "tailingSpace "))
